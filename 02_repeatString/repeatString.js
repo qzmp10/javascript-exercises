f47966db251195f990abefa54581317d9f2be587
qzmp10 <loganrobitaille@outlook.com>
2022-04-18 18:36:18 -0400

    Finished repeatString exercise using the .repeat() method. Also could have used a loop.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
 
-};
+
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        let repeatedString = string.repeat(num);
+        return repeatedString;
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
