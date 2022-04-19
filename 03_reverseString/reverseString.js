293006e104c2b9486d96396aaec7c6bd29d21297
qzmp10 <loganrobitaille@outlook.com>
2022-04-19 12:35:40 -0400

    Finished reverseString exercise, so much easier than yesterday.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    //convert input to string
+    let string = word.toString();
+    // create an array that holds all the characters
+    // of the input string 
+    let arrayOfStringCharacters = string.split("");
+    // reverse the characters in the array
+    let reverseArray = arrayOfStringCharacters.reverse();
+    // turn the array back into a string
+    let finalString = reverseArray.join("");
+    return finalString;
 };
 
 // Do not edit below this line
