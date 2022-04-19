9be6e711e7103939b0d1e6039da29da366059a10
qzmp10 <loganrobitaille@outlook.com>
2022-04-19 18:41:30 -0400

    Finished sumall exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) return "ERROR";
+    if (numOne < 0 || numTwo < 0) return "ERROR";
+    if (numOne > numTwo) {
+        const switcheroo = numOne;
+        numOne = numTwo;
+        numTwo = switcheroo;
+    }
+    let sumTotal = 0;
+    for (let i = numOne; i < numTwo + 1; i++) {
+        sumTotal += i;
+    } return sumTotal;
+}
 
-};
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
