8f61ae3b8af345604351b271b0f5c8f12472699c
qzmp10 <loganrobitaille@outlook.com>
2022-04-19 17:17:39 -0400

    Looked at solutions cause my head wanted to explode. FInished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
 
-};
+
+const removeFromArray = function(...args) {
+    let array = args[0];
+    // now filter the other args from the array
+    let filtered = array.filter(value => !args.includes(value))
+    return filtered;
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
